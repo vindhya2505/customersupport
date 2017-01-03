4572f295ae606cd02a8a500f448fb89482d7c149
vindhya2505 <vindhyaskonduru@rediffmail.com>
2017-01-03 17:06:32 +0530

    updated app.js

--- app.js
+++ app.js
@@ -68,7 +68,69 @@ bot.dialog('rootMenu', [
     }
 ]).reloadAction('showMenu', null, { matches: /^(menu|back)/i });
 
+bot.dialog('updateAddress', [
+    function (session, args) {
+        builder.Prompts.text(session, "Please enter new address details.")
+    },
+    function (session,results) {
 
+			switch (results.resonse.index) {
+            case 0:
+               // session.beginDialog('doorNumber');
+
+			 console.log("[200] " + req.method + " to " + req.url);
+          results.resonse.writeHead(200, "OK", {'Content-Type': 'text/html'});
+          results.resonse.write('<html><head><title>Hello<body>');
+          results.resonse.write('<h1>Welcomeo are you?</h1>');
+          results.resonse.write('<form="application/x-www-form-urlencoded" action="/formhandler" method="post">');
+          results.resonse.write('DoorNumber : <input type="text" name="dno" value="" /><br />');
+          results.resonse.write('Address1 :<input type="text" name="addr1" value="" /><br />');
+          results.resonse.write('<input type="submit"  value="submit"/>');
+          results.resonse.write('</form></body></html>');
+                break;
+            case 1:
+                session.beginDialog('address1');
+                break;
+            case 2:
+                session.beginDialog('city');
+                break;
+			case 3:
+				session.beginDialog('State');
+				break;
+            default:
+                session.endDialog();
+                break;
+        }
+        
+    }
+]);
+
+bot.dialog('doorNumber', [
+    function (session, args) {
+        builder.Prompts.number(session, "Enter Door Number")
+    },
+    function (session, results) {
+
+			switch (results.response.index) {
+            case 0:
+                session.beginDialog('doorNumber');
+                break;
+            case 1:
+                session.beginDialog('address1');
+                break;
+            case 2:
+                session.beginDialog('city');
+                break;
+			case 3:
+				session.beginDialog('State');
+				break;
+            default:
+                session.endDialog();
+                break;
+        }
+        
+    }
+]);
 /*bot.dialog('/', function (session) {
     session.send("Hello ! I can help you with 1. Address update, 2. Add vehicle to the Policy. Please choose the options 1..2..");
 });*/
